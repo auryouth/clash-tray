c83a8533653d639346fe6a043ef4ec68a5b57abe
auryouth <ausosa528@proton.me>
2024-03-31 14:14:08 +0800

    new: show Meta status with tooltip and icon color

--- clash-tray.py
+++ clash-tray.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+import threading
 import webbrowser
 
 import pystray
@@ -12,9 +13,12 @@ from win11toast import toast
 
 class ClashTray:
     def __init__(self):
-        self.tray_icon_path = self._resource_path("Meta.ico")
+        self.tray_icon_running_path = self._resource_path("Meta-running.ico")
+        self.tray_icon_stopped_path = self._resource_path("Meta.ico")
         self.icon = self._create_tray_icon()
         self.clash_process = None
+        self.timer = threading.Timer(5, self._check_clash_status)
+        self.timer.start()
 
     def _resource_path(self, relative_path):
         """Get absolute path to resource, works for dev and for PyInstaller"""
@@ -26,7 +30,7 @@ class ClashTray:
         return os.path.join(base_path, relative_path)
 
     def _notify(self, info):
-        toast("Clash Tray", info, icon=self.tray_icon_path, duration="short")
+        toast("Clash Tray", info, icon=self.tray_icon_stopped_path, duration="short")
 
     def _start_clash(self):
         try:
@@ -62,11 +66,25 @@ class ClashTray:
         webbrowser.open("https://d.metacubex.one")
 
     def _exit_program(self, icon):
+        self.timer.cancel()
         self._stop_clash()
         icon.stop()
 
+    def _check_clash_status(self):
+        self._update_tray_icon()
+        self.timer = threading.Timer(5, self._check_clash_status)
+        self.timer.start()
+
+    def _update_tray_icon(self):
+        if self.clash_process is not None and self.clash_process.poll() is None:
+            self.icon.icon = Image.open(self.tray_icon_running_path)
+            self.icon.title = "Clash Meta - 运行中"
+        else:
+            self.icon.icon = Image.open(self.tray_icon_stopped_path)
+            self.icon.title = "Clash Meta - 已停止"
+
     def _create_tray_icon(self):
-        tray_icon = Image.open(self.tray_icon_path)
+        tray_icon = Image.open(self.tray_icon_stopped_path)
         menu = Menu(
             item("启动 Clash", self._start_clash),
             item("打开网页", self._open_webpage, default=True),
